726b3b50e7506d3d2d8db88ddcb467e279050d9c
synthtech <synthtech0@gmail.com>
2016-12-23 17:49:25 -0500

    Update script

--- taiga-build.ps1
+++ taiga-build.ps1
@@ -3,62 +3,76 @@
 $vs = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 # Location of msbuild (for compiling Taiga)
 $msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
-# Location of libcurl source code
-$curl = "$env:USERPROFILE\Downloads\curl"
 # Location of Taiga source code
 $taiga = "$env:USERPROFILE\Documents\Git\taiga"
 
-
 $dir = $PSScriptRoot
 
-# Compiling libcurl
-$buildCurl = read-host "[Script] Build libcurl?"
-if ($buildCurl -eq "yes" -or $buildCurl -eq "y") {
-    write-output "[Script] Building libcurl..."
-    cd $curl\winbuild
-    Invoke-BatchFile $vs
-    nmake /f Makefile.vc mode=static RTLIBCFG=static VC=14
-    write-output "`n[Script] Build finished."
-    write-output "[Script] Copying library..."
-    robocopy /s /is $curl\builds\libcurl-vc14-x86-release-static-ipv6-sspi-winssl\lib $taiga\deps\lib\
-    write-output "[Script] Copy complete.`n"
-    cd $dir
+# Setup config
+$quickBuild = read-host "[Script] Quick build?"
+if ($quickBuild -eq "yes" -or $quickBuild -eq "y") {
+    $release = "release"
+    $build = "build"
+    $openBuild = "yes"
 }
+
+if (!($release)) {
+    $release = read-host "[Script] Build configuration? (release|debug)"
+    if ($release -eq "release" -or $release -eq "r") {
+        $release = "release"
+        write-output "[Script] Using release config."
+    } elseif ($release -eq "debug" -or $release -eq "d") {
+        $release = "debug"
+        write-output "[Script] Using debug config."
+    } else {
+        $release = "release"
+        write-output "[Script] Defaulting to release config."
+    }
+}
+
+if (!($build)) {
+    $build = read-host "[Script] Rebuild source code? (yes|no)"
+    if ($build -eq "yes" -or $build -eq "y") {
+        $build = "rebuild"
+        write-output "[Script] Rebuilding from source."
+    } elseif ($build -eq "no" -or $build -eq "n") {
+        $build = "build"
+        write-output "[Script] Building from source."
+    } else {
+        $build = "build"
+        write-output "[Script] Defaulting to build from source."
+    }
+}
+
+# Compiling libcurl
+cd $taiga\deps\src\curl\winbuild
+Invoke-BatchFile $vs
+
+if ($build -eq "rebuild") {
+    Remove-Item -Recurse -Force ..\builds
+}
+
+write-output "[Script] Building libcurl ($release)..."
+if ($release -eq "debug") { nmake /f Makefile.vc mode=static RTLIBCFG=static VC=14 DEBUG=yes }
+else { nmake /f Makefile.vc mode=static RTLIBCFG=static VC=14 }
+write-output "`n[Script] Build finished."
+write-output "[Script] Copying $release library..."
+robocopy /s /is ..\builds\libcurl-vc14-x86-$release-static-ipv6-sspi-winssl\lib $taiga\deps\lib
+write-output "[Script] Copy complete.`n"
+
+cd $dir
 
 # Compiling Taiga
-$release = read-host "[Script] Use Taiga's debug configuration?"
-if ($release -eq "yes" -or $release -eq "y") {
-    $release = "/p:Configuration=Debug"
-    write-output "[Script] Using debug config."
-} else {
-    $release = "/p:Configuration=Release"
-    write-output "[Script] Using release config."
-}
-
-$build = read-host "[Script] Rebuild Taiga from source?"
-if ($build -eq "yes" -or $build -eq "y") {
-    $build = "/t:Rebuild"
-    write-output "[Script] Rebuilding from source."
-} else {
-    $build = "/t:Build"
-    write-output "[Script] Building from source."
-}
-
 write-output "[Script] Building Taiga...`n"
-if ($msbuild) {
-    & $msbuild $taiga\project\vs2015\Taiga.vcxproj $build $release
-} else {
-    msbuild $taiga\project\vs2015\Taiga.vcxproj $build $release
-}
+if ($msbuild) { & $msbuild $taiga\project\vs2015\Taiga.vcxproj /t:$build /p:Configuration=$release }
+else { msbuild $taiga\project\vs2015\Taiga.vcxproj /t:$build /p:Configuration=$release }
 write-output "`n[Script] Build finished."
 
-$openBuild = read-host "[Script] Would you like to open the builds folder?"
+if (!($openBuild)) {
+    $openBuild = read-host "[Script] Would you like to open the builds folder?"
+}
 if ($openBuild -eq "yes" -or $openBuild -eq "y") {
-    if ($release -eq "/p:Configuration=Debug") {
-        Invoke-Item $taiga\bin\Debug
-    } else {
-        Invoke-Item $taiga\bin\Release
-    }
+    Invoke-Item $taiga\bin\$release
 }
 
 read-host -prompt "`n[Script] Press enter to exit"
