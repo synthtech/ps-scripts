3894a813a6e26de26ce2ba88f80f8113400b4f90
synthtech <synthtech0@gmail.com>
2017-02-21 15:34:57 -0500

    Show more output

--- sublister.ps1
+++ sublister.ps1
@@ -5,7 +5,7 @@ $dir = $PSScriptRoot
 while(!($domain)) { $domain = read-host "[Script] Enter a domain" }
 
 cd $source
-python sublist3r.py -e baidu,yahoo,google,bing,ask,netcraft,ssl -d $domain
+python sublist3r.py -v -e baidu,yahoo,google,bing,ask,netcraft,ssl -d $domain
 
 read-host -prompt "`n[Script] Press enter to exit"
 cd $dir
