a48a420d81a50ab4efd1143f18b9c352db103aa0
synthtech <synthtech0@gmail.com>
2017-10-30 01:14:25 -0400

    Cleanup taiga-build

--- taiga-build.ps1
+++ taiga-build.ps1
@@ -1,85 +1,94 @@
 # Location of vcvars32.bat (for compiling libcurl)
-$vs = "${env:PROGRAMFILES(X86)}\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+$VcVars = "${env:PROGRAMFILES(X86)}\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 # Location of MsBuild (for compiling Taiga)
-$msbuild = "${env:PROGRAMFILES(X86)}\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+$MsBuild = "${env:PROGRAMFILES(X86)}\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 # Location of Taiga source code
-$source = "$env:USERPROFILE\Documents\Git\taiga"
+$Source = "$env:USERPROFILE\Documents\Git\taiga"
 # Location of Taiga installation
-$install = "$env:APPDATA\Taiga"
+$Install = "$env:APPDATA\Taiga"
 # Windows version
-$platform = "10.0.16299.0"
+$Platform = "10.0.16299.0"
 
-$dir = $PSScriptRoot
+$Dir = $PSScriptRoot
 
 # Setup config
-$quickBuild = read-host "[Script] Quick build?"
-if ($quickBuild -eq "yes" -or $quickBuild -eq "y") {
-    $release = "release"
-    $build = "build"
-    $copyBuild = "yes"
+$QuickBuild = Read-Host "[taiga-build] Quick build?"
+if ($QuickBuild -eq "yes" -or $QuickBuild -eq "y") {
+    $Release = "release"
+    $Build = "build"
+    $CopyBuild = "yes"
 }
 
-if (!($release)) {
-    $release = read-host "[Script] Build configuration? (release|debug)"
-    if ($release -eq "release" -or $release -eq "r") {
-        $release = "release"
-        write-output "[Script] Using release config."
-    } elseif ($release -eq "debug" -or $release -eq "d") {
-        $release = "debug"
-        write-output "[Script] Using debug config."
+if (!($Release)) {
+    $Release = Read-Host "[taiga-build] Build configuration? (release|debug)"
+    if ($Release -eq "release" -or $Release -eq "r") {
+        $Release = "release"
+        Write-Output "[taiga-build] Using release config."
+    } elseif ($Release -eq "debug" -or $Release -eq "d") {
+        $Release = "debug"
+        Write-Output "[taiga-build] Using debug config."
     } else {
-        $release = "release"
-        write-output "[Script] Defaulting to release config."
+        $Release = "release"
+        Write-Output "[taiga-build] Defaulting to release config."
     }
 }
 
-if (!($build)) {
-    $build = read-host "[Script] Rebuild source code? (yes|no)"
-    if ($build -eq "yes" -or $build -eq "y") {
-        $build = "rebuild"
-        write-output "[Script] Rebuilding from source."
-    } elseif ($build -eq "no" -or $build -eq "n") {
-        $build = "build"
-        write-output "[Script] Building from source."
+if (!($Build)) {
+    $Build = Read-Host "[taiga-build] Rebuild source code? (yes|no)"
+    if ($Build -eq "yes" -or $Build -eq "y") {
+        $Build = "rebuild"
+        Write-Output "[taiga-build] Rebuilding from source."
+    } elseif ($Build -eq "no" -or $Build -eq "n") {
+        $Build = "build"
+        Write-Output "[taiga-build] Building from source."
     } else {
-        $build = "build"
-        write-output "[Script] Defaulting to build from source."
+        $Build = "build"
+        Write-Output "[taiga-build] Defaulting to build from source."
     }
 }
 
 # Compiling libcurl
-cd $source\deps\src\curl\winbuild
-Invoke-BatchFile $vs
-Invoke-BatchFile $source\deps\src\curl\buildconf.bat
+Set-Location $Source\deps\src\curl\winbuild
+Invoke-BatchFile $VcVars
+Invoke-BatchFile $Source\deps\src\curl\buildconf.bat
 
-if ($build -eq "rebuild") {
+if ($Build -eq "rebuild") {
     Remove-Item -Recurse -Force ..\builds
 }
 
-write-output "[Script] Building libcurl ($release)..."
-if ($release -eq "debug") { nmake /f Makefile.vc mode=static RTLIBCFG=static VC=15 MACHINE=x86 DEBUG=yes }
-else { nmake /f Makefile.vc mode=static RTLIBCFG=static MACHINE=x86 VC=15 }
-write-output "`n[Script] Build finished."
-write-output "[Script] Copying $release library..."
-robocopy /s /is ..\builds\libcurl-vc15-x86-$release-static-ipv6-sspi-winssl\lib $source\deps\lib
-write-output "[Script] Copy complete.`n"
+Write-Output "[taiga-build] Building libcurl ($Release)..."
+if ($Release -eq "debug") {
+    nmake /f Makefile.vc mode=static RTLIBCFG=static VC=15 MACHINE=x86 DEBUG=yes
+}
+else {
+    nmake /f Makefile.vc mode=static RTLIBCFG=static MACHINE=x86 VC=15
+}
+Write-Output "`n[taiga-build] Build finished."
+Write-Output "[taiga-build] Copying $Release library..."
+robocopy /s /is ..\builds\libcurl-vc15-x86-$Release-static-ipv6-sspi-winssl\lib $Source\deps\lib
+Write-Output "[taiga-build] Copy complete.`n"
 
-cd $dir
+Set-Location $Dir
 
 # Compiling Taiga
-write-output "[Script] Building Taiga...`n"
-if ($msbuild) { & $msbuild $source\project\vs2017\Taiga.vcxproj /t:$build /p:Configuration=$release /p:WindowsTargetPlatformVersion=$platform }
-else { msbuild $source\project\vs2017\Taiga.vcxproj /t:$build /p:Configuration=$release /p:WindowsTargetPlatformVersion=$platform }
-write-output "`n[Script] Build finished."
-
-if (!($copyBuild)) {
-    $copyBuild = read-host "[Script] Would you like to copy the build to your Taiga folder?"
+Write-Output "[taiga-build] Building Taiga...`n"
+if ($MsBuild) {
+    & $MsBuild $Source\project\vs2017\Taiga.vcxproj /t:$Build /p:Configuration=$Release /p:WindowsTargetPlatformVersion=$Platform
 }
-if ($copyBuild -eq "yes" -or $copyBuild -eq "y") {
+else {
+    MsBuild $Source\project\vs2017\Taiga.vcxproj /t:$Build /p:Configuration=$Release /p:WindowsTargetPlatformVersion=$Platform
+}
+Write-Output "`n[taiga-build] Build finished."
+
+# Copy executable
+if (!($CopyBuild)) {
+    $CopyBuild = Read-Host "[taiga-build] Would you like to copy the build to your Taiga folder?"
+}
+if ($CopyBuild -eq "yes" -or $CopyBuild -eq "y") {
     if (Get-Process taiga -ErrorAction SilentlyContinue) {
         Stop-Process -Name taiga -Force
     }
-    Copy-Item $source\bin\$release\Taiga.exe -destination $install
+    Copy-Item $Source\bin\$Release\Taiga.exe -Destination $Install
 }
 
-read-host -prompt "`n[Script] Press enter to exit"
+Read-Host -Prompt "`n[taiga-build] Press any key to exit"
