82e0e9d023c4a19da47726a2e947c039cdbb3272
synthtech <synthtech0@gmail.com>
2018-01-24 17:04:32 -0500

    Update sublister

--- sublister.ps1
+++ sublister.ps1
@@ -1,11 +1,13 @@
 # Location of Sublist3r script
-$source = "$env:USERPROFILE\Documents\Git\Sublist3r"
-$dir = $PSScriptRoot
+$Source = "$env:USERPROFILE\Documents\Git\Sublist3r"
+$Dir = $PSScriptRoot
 
-while(!($domain)) { $domain = read-host "[Script] Enter a domain" }
+while(!($Domain)) {
+    $Domain = Read-Host "[sublister] Enter a domain"
+}
 
-cd $source
-python sublist3r.py -v -e baidu,yahoo,google,bing,ask,netcraft,ssl -d $domain
+Set-Location $Source
+./sublist3r.py -v -e baidu,yahoo,google,bing,ask,netcraft -d $Domain
 
-read-host -prompt "`n[Script] Press enter to exit"
-cd $dir
+Read-Host -Prompt "`n[sublister] Press any key to exit"
+Set-Location $Dir
