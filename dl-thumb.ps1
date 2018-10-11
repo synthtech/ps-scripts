85d72c33c0b21836d773ddb56ea2d639476ed29b
synthtech <synthtech0@gmail.com>
2018-10-11 15:38:57 -0400

    Cleanup dl scripts

--- dl-thumb.ps1
+++ dl-thumb.ps1
@@ -1,4 +1,3 @@
-# User agent
 $agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
 
 while(!($url)) { $url = read-host "Enter the url" }
--- dl-video.ps1
+++ dl-video.ps1
@@ -1,16 +1,6 @@
-# User agent
 $agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
-# Default download directory
-$default = "$env:USERPROFILE\Videos"
 
-$dir = $PSScriptRoot
-
-$dl = read-host "Enter the directory"
-if (!($dl)) { $dl = $default }
 while(!($url)) { $url = read-host "Enter the url" }
 
-cd $dl
 if ($url -match "nicovideo.jp") { youtube-dl --user-agent $agent --netrc $url }
 else { youtube-dl --user-agent $agent $url }
-
-cd $dir
