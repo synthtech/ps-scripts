e9dd56406d9c993045d445c848515dea068fdb69
synthtech <synthtech0@gmail.com>
2021-02-12 02:02:58 -0500

    build taiga x64

--- taiga-build.ps1
+++ taiga-build.ps1
@@ -1,7 +1,7 @@
 # Find Visual Studio install path using VSSetup
 $VsPath = (Get-VSSetupInstance | Select-VSSetupInstance -Version 16.0).InstallationPath
-# Location of vcvars32.bat (for compiling libcurl)
-$VcVars = "$VsPath\VC\Auxiliary\Build\vcvars32.bat"
+# Location of vcvars64.bat (for compiling libcurl)
+$VcVars = "$VsPath\VC\Auxiliary\Build\vcvars64.bat"
 # Location of MsBuild (for compiling Taiga)
 $MsBuild = "$VsPath\MSBuild\Current\Bin\MSBuild.exe"
 # Location of Taiga source code
@@ -27,7 +27,7 @@ function Invoke-BatchFile {
 }
 
 # Setup config
-$QuickBuild = Read-Host "[taiga-build] Quick build?"
+$QuickBuild = Read-Host "[Taiga] Quick build?"
 if ($QuickBuild -eq "yes" -or $QuickBuild -eq "y") {
     $Release = "release"
     $Build = "build"
@@ -36,37 +36,37 @@ if ($QuickBuild -eq "yes" -or $QuickBuild -eq "y") {
 }
 
 if (!($Release)) {
-    $Release = Read-Host "[taiga-build] Build configuration? (release|debug)"
+    $Release = Read-Host "[Taiga] Build configuration? (release|debug)"
     if ($Release -eq "release" -or $Release -eq "r") {
         $Release = "release"
-        Write-Output "[taiga-build] Using release config."
+        Write-Output "[Taiga] Using release config."
     } elseif ($Release -eq "debug" -or $Release -eq "d") {
         $Release = "debug"
-        Write-Output "[taiga-build] Using debug config."
+        Write-Output "[Taiga] Using debug config."
     } else {
         $Release = "release"
-        Write-Output "[taiga-build] Defaulting to release config."
+        Write-Output "[Taiga] Defaulting to release config."
     }
 }
 
 if (!($Build)) {
-    $Build = Read-Host "[taiga-build] Rebuild source code? (yes|no|curl)"
+    $Build = Read-Host "[Taiga] Rebuild source code? (yes|no|curl)"
     if ($Build -eq "yes" -or $Build -eq "y") {
         $Build = "rebuild"
         $CurlBuild = "no"
-        Write-Output "[taiga-build] Rebuilding from source."
+        Write-Output "[Taiga] Rebuilding from source."
     } elseif ($Build -eq "no" -or $Build -eq "n") {
         $Build = "build"
         $CurlBuild = "no"
-        Write-Output "[taiga-build] Building from source."
+        Write-Output "[Taiga] Building from source."
     } elseif ($Build -eq "curl") {
         $Build = "rebuild"
         $CurlBuild = "yes"
-        Write-Output "[taiga-build] Rebuilding from source including curl."
+        Write-Output "[Taiga] Rebuilding from source including curl."
     } else {
         $Build = "build"
         $CurlBuild = "no"
-        Write-Output "[taiga-build] Defaulting to build from source."
+        Write-Output "[Taiga] Defaulting to build from source."
     }
 }
 
@@ -81,40 +81,40 @@ if ($CurlBuild -eq "yes") {
     }
 }
 
-Write-Output "[taiga-build] Building libcurl ($Release)..."
+Write-Output "[Taiga] Building libcurl ($Release)..."
 if ($Release -eq "debug") {
-    nmake /f Makefile.vc mode=static RTLIBCFG=static VC=16 MACHINE=x86 DEBUG=yes
+    nmake /f Makefile.vc mode=static RTLIBCFG=static VC=16 MACHINE=x64 DEBUG=yes
 }
 else {
-    nmake /f Makefile.vc mode=static RTLIBCFG=static VC=16 MACHINE=x86
+    nmake /f Makefile.vc mode=static RTLIBCFG=static VC=16 MACHINE=x64
 }
-Write-Output "`n[taiga-build] Build finished."
-Write-Output "[taiga-build] Copying $Release library..."
-robocopy /s /is ..\builds\libcurl-vc16-x86-$Release-static-ipv6-sspi-schannel\lib $Source\deps\lib
-Write-Output "[taiga-build] Copy complete.`n"
+Write-Output "`n[Taiga] Build finished."
+Write-Output "[Taiga] Copying $Release library..."
+robocopy /s /is ..\builds\libcurl-vc16-x64-$Release-static-ipv6-sspi-schannel\lib $Source\deps\lib\x64
+Write-Output "[Taiga] Copy complete.`n"
 
 Set-Location $Dir
 
 # Compiling Taiga
-Write-Output "[taiga-build] Building Taiga...`n"
+Write-Output "[Taiga] Building Taiga...`n"
 if ($MsBuild) {
-    & $MsBuild $Source\project\vs2019\Taiga.vcxproj /t:$Build /p:Configuration=$Release
+    & $MsBuild $Source\project\vs2019\Taiga.vcxproj /t:$Build /p:Configuration=$Release /p:Platform=x64
 }
 else {
-    MSBuild $Source\project\vs2019\Taiga.vcxproj /t:$Build /p:Configuration=$Release
+    MSBuild $Source\project\vs2019\Taiga.vcxproj /t:$Build /p:Configuration=$Release /p:Platform=x64
 }
-Write-Output "`n[taiga-build] Build finished."
+Write-Output "`n[Taiga] Build finished."
 
 # Copy executable
 if (!($CopyBuild)) {
-    $CopyBuild = Read-Host "[taiga-build] Would you like to copy the build to your Taiga folder?"
+    $CopyBuild = Read-Host "[Taiga] Would you like to copy the build to your Taiga folder?"
 }
 if ($CopyBuild -eq "yes" -or $CopyBuild -eq "y") {
     if (Get-Process taiga -ErrorAction SilentlyContinue) {
         Stop-Process -Name taiga -Force
     }
-    Copy-Item $Source\bin\$Release\Taiga.exe -Destination $Install
+    Copy-Item $Source\bin\x64\$Release\Taiga.exe -Destination $Install
 }
 
-Read-Host -Prompt "`n[taiga-build] Press any key to exit"
+Read-Host -Prompt "`n[Taiga] Press any key to exit"
 Set-Location $Dir
