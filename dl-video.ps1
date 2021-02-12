ce5fc7b8a5a6532ce0f905d0030baea076bd5da5
synthtech <synthtech0@gmail.com>
2021-02-12 00:32:05 -0500

    Delete dl-video.ps1

--- dl-video.ps1
+++
@@ -1,5 +0,0 @@
-$agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
-
-while(!($url)) { $url = read-host "Enter the url" }
-
-youtube-dlc --user-agent $agent --netrc -v $url
